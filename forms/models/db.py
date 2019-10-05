e23aac30302be1a473e3d8ee56a90ca9e0fd1d9f
yunlongYoung <dragonlip@qq.com>
2019-10-05 11:39:42 +0800

    做了一点新的构思，

--- forms/models/db.py
+++ forms/models/db.py
@@ -26,16 +26,51 @@ class Num(Base):
     option_B = Column(Text)
     option_C = Column(Text)
     option_D = Column(Text)
-    # 从试卷的大分类到最小的分类，这些标签只会在解析时出现
+    # 从试卷的大分类到最小的分类
+    category_0 = Column(Text)
+    category_1 = Column(Text)
+    category_2 = Column(Text)
+    # 这些标签只会在解析时出现
     # 这个是实现精确分类和精准分析的关键
+    # ! 进行交卷分析时，需要打上错误，未做，已做，蒙的，地区（国考或四川、山东、湖北等标签）
+    # ! 这些提前能想到的是否直接做成表，可以存储其他信息，比如wrong，overtime等
+    label_region = Column(Text)
+    label_finished = Column(Text, default="未做")
+    label_error = Column(Text)
+    label_guess = Column(Text)
     label_0 = Column(Text)
     label_1 = Column(Text)
     label_2 = Column(Text)
     label_3 = Column(Text)
+    label_4 = Column(Text)
 
 
-class Record(Base):
-    __tablename__ = "record"
+class Virtual_Paper(Base):
+    # TODO 如何保存练习题的信息，从几套题中抽取的几道题
+    # TODO 必须把current和virtual_paper分离
+    # 当前在做的试卷信息
+    __tablename__ = "virtual_paper"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    # 当前试卷
+    # 当前题号
+    # 当前试卷开始时间，可以查最后时间做的试卷，并且是否完结
+    # 当前试卷总用时
+    # 是否交卷
+    paper_id = Column(Integer, ForeignKey("test_paper.id"))
+    current_num = Column(Integer)
+    start_datetime = Column(Integer, index=True)
+    totaltime = Column(Integer, primary_key=True)
+    finished = Column(Boolean, default=False)
+
+
+class Paper_Record(Base):
+    __tablename__ = "paper_record"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+
+
+class Num_Record(Base):
+    # ! 题号经过抽象，比如练习题中的题号不一定是真题中的题号
+    __tablename__ = "num_record"
     id = Column(Integer, primary_key=True, autoincrement=True)
     virtual_paper_id = Column(Integer, ForeignKey("virtual_paper.id"))
     num_id = Column(Integer, ForeignKey("num.id"), index=True)
@@ -44,8 +79,8 @@ class Record(Base):
     note = Column(Text)
 
 
-class Operation(Base):
-    __tablename__ = "operation"
+class Num_Operation(Base):
+    __tablename__ = "num_operation"
     id = Column(Integer, primary_key=True, autoincrement=True)
     virtual_paper_id = Column(Integer, ForeignKey("virtual_paper.id"))
     num_id = Column(Integer, ForeignKey("num.id"), index=True)
@@ -62,26 +97,17 @@ class Operation(Base):
     datetime = Column(Integer, index=True)
 
 
-class Virtual_Paper(Base):
-    # TODO 想保存所有的做试卷的记录
-    # 当前在做的试卷信息
-    __tablename__ = "virtual_paper"
+class Overtime(Base):
+    __tablename__ = "overtime"
     id = Column(Integer, primary_key=True, autoincrement=True)
-    # 当前试卷
-    # 当前题号
-    # 当前试卷开始时间，可以查最后时间做的试卷，并且是否完结
-    # 当前试卷总用时
-    # 是否交卷
-    paper_id = Column(Integer, ForeignKey("test_paper.id"))
-    current_num = Column(Integer)
-    start_datetime = Column(Integer, index=True)
-    totaltime = Column(Integer, primary_key=True)
-    finished = Column(Boolean, default=False)
+    virtual_paper_id = Column(Integer, ForeignKey("virtual_paper.id"))
+    num_id = Column(Integer, ForeignKey("num.id"), index=True)
 
 
 class Wrong(Base):
     # 错误问题
     __tablename__ = "wrong"
+    id = Column(Integer, primary_key=True, autoincrement=True)
     virtual_paper_id = Column(Integer, ForeignKey("virtual_paper.id"))
     num_id = Column(Integer, ForeignKey("num.id"), index=True)
     # TODO 整数转原因
@@ -91,6 +117,7 @@ class Wrong(Base):
 class Slow(Base):
     # 答的慢的问题
     __tablename__ = "slow"
+    id = Column(Integer, primary_key=True, autoincrement=True)
     virtual_paper_id = Column(Integer, ForeignKey("virtual_paper.id"))
     num_id = Column(Integer, ForeignKey("num.id"), index=True)
     # TODO 整数转原因

48af81435841dc113a61b09230e3bce81af8f1d4
yunlongYoung <dragonlip@qq.com>
2019-10-16 11:26:24 +0800

    增加了一个enum实例文件

--- forms/models/db.py
+++ forms/models/db.py
@@ -7,6 +7,10 @@ Base = declarative_base()
 
 
 class Test_Paper(Base):
+    """试卷的抽象，和Num放在一起也可以
+    分离开比较简单方便
+    """
+
     __tablename__ = "test_paper"
     id = Column(Integer, primary_key=True, autoincrement=True)
     test_kind = Column(String(2), default="行测")
@@ -16,6 +20,10 @@ class Test_Paper(Base):
 
 
 class Num(Base):
+    """问题的抽象，包括题号、题干、选项
+    还包括问题的分类、标签等
+    """
+
     __tablename__ = "num"
     id = Column(Integer, primary_key=True, autoincrement=True)
     paper_id = Column(Integer, ForeignKey("test_paper.id"))
@@ -26,6 +34,16 @@ class Num(Base):
     B = Column(Text)
     C = Column(Text)
     D = Column(Text)
+
+
+class Num_Property(Base):
+    """ 因为刚开始打开试卷时，不需要读取分类和标签
+    只有后面交卷后才需要分析，所以可以和试卷分开
+    增加一个Num_Property
+    # ! 如果这个可以动态增加就好了
+    """
+
+    num_id = Column(Integer, ForeignKey("num.id"), primary_key=True)
     # 从试卷的大分类到最小的分类
     category_0 = Column(Text)
     category_1 = Column(Text)
@@ -34,14 +52,29 @@ class Num(Base):
     # 这个是实现精确分类和精准分析的关键
     # ! 进行交卷分析时，需要打上错误，未做，已做，蒙的，地区（国考或四川、山东、湖北等标签）
     # ! 这些提前能想到的是否直接做成表，可以存储其他信息，比如wrong，overtime等
-    label_finished = Column(Text)
-    label_error = Column(Text)
-    label_guess = Column(Text)
-    label_0 = Column(Text)
-    label_1 = Column(Text)
-    label_2 = Column(Text)
-    label_3 = Column(Text)
-    label_4 = Column(Text)
+    finished = Column(Text)
+    guessed = Column(Text)
+    # ! 根据需要增加，分为考试属性和固有属性
+
+
+class True_Paper(Base):
+    """这个表用于存放做过的真题记录
+    也就是一套一套做的卷子，
+    而不是练习模式或者专项模式做的
+    """
+
+    __tablename__ = "true_paper"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    # 当前试卷
+    # 当前题号
+    # 当前试卷开始时间，可以查最后时间做的试卷，并且是否完结
+    # 当前试卷总用时
+    # 是否交卷
+    paper_id = Column(Integer, ForeignKey("test_paper.id"))
+    start_datetime = Column(Integer, index=True)
+    totaltime = Column(Integer, primary_key=True)
+    last_num = Column(Integer)
+    finished = Column(Boolean, default=False)
 
 
 class Virtual_Paper(Base):
@@ -50,28 +83,30 @@ class Virtual_Paper(Base):
     # 当前在做的试卷信息
     __tablename__ = "virtual_paper"
     id = Column(Integer, primary_key=True, autoincrement=True)
-    # 当前试卷
-    # 当前题号
-    # 当前试卷开始时间，可以查最后时间做的试卷，并且是否完结
-    # 当前试卷总用时
-    # 是否交卷
-    paper_id = Column(Integer, ForeignKey("test_paper.id"))
-    current_num = Column(Integer)
+    last_num = Column(Integer)
     start_datetime = Column(Integer, index=True)
     totaltime = Column(Integer, primary_key=True)
     finished = Column(Boolean, default=False)
 
 
-class Paper_Record(Base):
-    __tablename__ = "paper_record"
+class Virtual_Num(Base):
+    """存放所有曾经生成过的虚拟试卷，比如专项练习等
+    意思是这张虚拟试卷的题，由这些题组成
+    """
+
+    __tablename__ = "virtual_num"
     id = Column(Integer, primary_key=True, autoincrement=True)
+    virtual_paper_id = Column(Integer, ForeignKey("virtual_paper.id"))
+    num_id = Column(Integer, ForeignKey("num.id"))
 
 
 class Num_Record(Base):
     # ! 题号经过抽象，比如练习题中的题号不一定是真题中的题号
     __tablename__ = "num_record"
     id = Column(Integer, primary_key=True, autoincrement=True)
-    virtual_paper_id = Column(Integer, ForeignKey("virtual_paper.id"))
+    # ! 难点是这是张虚拟试卷呢，还是真题呢
+    # ! 难道说不能用外键吗，只能存Integer吗
+    paper_id = Column(Integer)
     num_id = Column(Integer, ForeignKey("num.id"), index=True)
     chosen = Column(Integer, default=-1)
     question_time = Column(Integer, default=0)
@@ -81,17 +116,18 @@ class Num_Record(Base):
 class Num_Operation(Base):
     __tablename__ = "num_operation"
     id = Column(Integer, primary_key=True, autoincrement=True)
-    virtual_paper_id = Column(Integer, ForeignKey("virtual_paper.id"))
+    # !既可能是真题，也可能是虚拟试卷
+    paper_id = Column(Integer)
     num_id = Column(Integer, ForeignKey("num.id"), index=True)
-    # ! 把操作转换为数字
-    # ! 0: quit query
-    # ! 1: open query
-    # ! 2: previous question
-    # ! 3: next quesiton
-    # ! 4: pause question
-    # ! 6: continue question
-    # ! 8: commit query
-    # ! 9: passive start
+    # 把操作转换为数字
+    # 0: quit query
+    # 1: open query
+    # 2: previous question
+    # 3: next quesiton
+    # 4: pause question
+    # 6: continue question
+    # 8: commit query
+    # 9: passive start
     operation = Column(Integer)
     datetime = Column(Integer, index=True)
 
@@ -99,7 +135,8 @@ class Num_Operation(Base):
 class Overtime(Base):
     __tablename__ = "overtime"
     id = Column(Integer, primary_key=True, autoincrement=True)
-    virtual_paper_id = Column(Integer, ForeignKey("virtual_paper.id"))
+    # !既可能是真题，也可能是虚拟试卷
+    paper_id = Column(Integer)
     num_id = Column(Integer, ForeignKey("num.id"), index=True)
 
 
@@ -107,7 +144,8 @@ class Wrong(Base):
     # 错误问题
     __tablename__ = "wrong"
     id = Column(Integer, primary_key=True, autoincrement=True)
-    virtual_paper_id = Column(Integer, ForeignKey("virtual_paper.id"))
+    # !既可能是真题，也可能是虚拟试卷
+    paper_id = Column(Integer)
     num_id = Column(Integer, ForeignKey("num.id"), index=True)
     # TODO 整数转原因
     wrong_reason = Column(Integer)
@@ -117,7 +155,8 @@ class Slow(Base):
     # 答的慢的问题
     __tablename__ = "slow"
     id = Column(Integer, primary_key=True, autoincrement=True)
-    virtual_paper_id = Column(Integer, ForeignKey("virtual_paper.id"))
+    # !既可能是真题，也可能是虚拟试卷
+    paper_id = Column(Integer)
     num_id = Column(Integer, ForeignKey("num.id"), index=True)
     # TODO 整数转原因
     slow_reason = Column(Integer)
---
+++ forms/models/enums.py
@@ -0,0 +1,6 @@
+from enum import Enum, auto
+
+
+class _testkinds(Enum):
+    XINGCE = 1
+    SHENLUN = 2
